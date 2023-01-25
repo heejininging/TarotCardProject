7fd2a40f0e35f91108710d2dcdc107851884e244
nabom921126 <nabom921126@gmail.com>
2023-01-25 18:40:47 +0900

    img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import GenderChoice from './Hayoung/GenderChoice';
 
 function App() {
   return (
     <div className="App">
       <h3>메인 페이지 메인페이지~</h3>
+      <GenderChoice/>
     </div>
   );
 }
---
+++ src/Hayoung/GenderChoice.js
@@ -0,0 +1,14 @@
+
+const GenderChoice = () => {
+
+    return (
+        <>  
+            <div className="main">
+                <img src="img/tarot-pj.png" alt="logo"/>
+
+            </div>
+        </>
+    )
+}
+
+export default GenderChoice;
