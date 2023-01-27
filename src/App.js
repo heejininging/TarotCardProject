20d47ae034cce657ffa04b71473136f7f0b190af
kerri <kerri.shin@gmail.com>
2023-01-27 11:00:32 +0900

    20230127 1100 Jisoo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import GenderChoice from './Hayoung/GenderChoice';
+import Home_J from './Jisoo/Home_J';
 
 function App() {
   return (
     <div className="App">
       <h3>메인 페이지 메인페이지~</h3>
       <GenderChoice/>
+      <Home_J/>
     </div>
   );
 }
